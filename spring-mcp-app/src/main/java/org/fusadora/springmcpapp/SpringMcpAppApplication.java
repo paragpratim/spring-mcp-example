68f249043963cef590d12d70e3fa3663021a5bf3
Parag Ghosh <paragpratim@gmail.com>
2026-08-14 13:01:36 +0200

    Add initial Spring MCP application setup with Docker and Maven support

---
+++ spring-mcp-app/src/main/java/org/fusadora/springmcpapp/SpringMcpAppApplication.java
@@ -0,0 +1,13 @@
+package org.fusadora.springmcpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMcpAppApplication.class, args);
+    }
+
+}
---
+++ spring-mcp-app/src/test/java/org/fusadora/springmcpapp/SpringMcpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fusadora.springmcptoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
